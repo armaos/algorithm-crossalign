341dc22f5d1d6ed54caa32a8aff952e3cfc82b4f
irene <irene@Nievess-iMac.local>
2017-11-24 15:14:09 +0100

    overlap

--- template/overlap.r
+++ template/overlap.r
@@ -1,7 +1,6 @@
 a<-read.table("cross_short.txt")
 b<-read.table("cross_long.txt")
-png("/outputs/plot2.png")
-#plot(smooth.spline(a$V2),type="l",col=2,lwd=3,ylim=c(-1,1),xlab="Sequence",ylab="Score",main="RSS propensity")
+png("outputs/plot2.png")
 plot(b$V2,type="l",col=2,lwd=3,ylim=c(-1,1),xlab="Sequence",ylab="CROSS score",main="")
 lines(a$V2,col="darkgray",lwd=3)
 abline(h=0,lty="dashed", col="black",lwd=2)
