1fae2a4300dabadf75dfe9865ce9b0d7e59e360b
irene <irene@Nievess-iMac.local>
2017-11-24 14:56:03 +0100

    overlap

--- template/dtw_obe.r
+++ template/dtw_obe.r
@@ -7,7 +7,7 @@ name2<-as.character(args[8])
 
 library(dtw)
 library(proxy)
-png("outputs/plot.png")
+#png("outputs/plot.png")
 one<-read.table(vect1)
 two<-read.table(vect2)
 alignment<-dtw(one$V1,two$V1,keep=TRUE,open.begin=TRUE,open.end=TRUE,step.pattern=asymmetric,dist.method="Manhattan")
@@ -15,7 +15,7 @@ name1
 name2
 alignment$normalizedDistance
 alignment$index2
-plot(alignment,type="threeway",col=2,lwd=3,xlab="CROSS profile 1",ylab="CROSS profile 2",main="Structural alignment")
+#plot(alignment,type="threeway",col=2,lwd=3,xlab="CROSS profile 1",ylab="CROSS profile 2",main="Structural alignment")
 dev.off()
 
 #OBE
