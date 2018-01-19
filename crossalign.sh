59868a4aa7ed14a4ca6a27138661fe40f6354a9b
ggt-blackmore <webservice+blackmore@tartaglialab.com>
2018-01-19 18:44:14 +0100

    fixing alignment

--- crossalign.sh
+++ crossalign.sh
@@ -34,11 +34,13 @@ then
 	sed 's/]/-/g' dtw_output.tmp | awk '(NF>2 && $1~/-/){$1=""; print $0}' > outputs/matches.txt
 	#for i in `awk '{print $0}' ./outputs/matches.txt | tr " " "\n" | awk '($1!~/]/)' | awk '(length($1)>0)'`; do awk '(NR=="'$i'")' shorter.txt; done > cross_short.txt
 	cp shorter.txt cross_short.txt
+	length=`wc cross_short.txt | awk '{print $1}'`
+	for ((i=1;i<=$length;i++));do pos=`awk '(NR=="'$i'")' matches.col`; awk '(NR=='$pos')' longer.txt | awk '{print "'$i'", $2}'; done > cross_long.txt
 	start0=$(head -n 1 ./outputs/matches.txt | awk '{print $1}')
 	end0=$(wc cross_short.txt | awk '{print $1}')
 	final0=$(($end0+$start0))
 	echo $final0 > outputs/end.txt
-	awk -v start=$start0 -v end=$final0 '($1>=start && $1<=end)' longer.txt > cross_long.txt
+	#awk -v start=$start0 -v end=$final0 '($1>=start && $1<=end)' longer.txt > cross_long.txt
 	head -n 1 ./outputs/matches.txt | awk '{print $1}' > outputs/start.txt
 	Rscript overlap.r
 fi
--- template/overlap.r
+++ template/overlap.r
@@ -1,8 +1,7 @@
 a<-read.table("cross_short.txt")
 b<-read.table("cross_long.txt")
 png("outputs/plot2.png")
-plot(b$V2,type="l",col=2,lwd=3,ylim=c(-1,1),xlab="Sequence",ylab="CROSS score",main="")
-lines(a$V2,col="darkgray",lwd=3)
+plot(b$V2,type="l",col=2,lwd=1.5,ylim=c(-1,1),xlab="Sequence",ylab="CROSS score",main="")
+lines(a$V2,col="darkgray",lwd=1.5)
 abline(h=0,lty="dashed", col="black",lwd=2)
 dev.off()
-
